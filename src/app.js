f0ca5c9f680aaaa2b1a15bdcd7e261077d1bd4cd
Vedant-07 <vedantnpatel7@gmail.com>
2024-12-04 19:40:04 +0530

    practised routing , middlewares & error handling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-const expresso = require("express");
+const express = require("express");
 
-const app = expresso();
+const app = express();
 
-app.use("/check", (req, res) => {
-  res.send("hi again");
+// app.use("/user", (req, res) => {
+//   res.send("hi again");
+// });
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    console.log("got and errror");
+    res.status(404).send("&&&&&&&&&&&&&&&&&&&&&&");
+  }
+  console.log("hang ho gaya isnt ???");
+  //next();
 });
 
-app.listen(7777,"localhost", () => {
+app.get("/user", (req, res) => {
+  //res.send("user get");
+  console.log("chalo bhai");
+  res.send("done for now");
+});
+
+//for the dynamic parameter
+app.post("/random/:usr", (req, res) => {
+  console.log(req.params);
+});
+
+//for the parameter which has query fields ,that is the ?
+app.post("/posting", (req, res) => {
+  console.log(req.query);
+  res.send("user info send here");
+});
+
+app.listen(7777, "localhost", () => {
   console.log("Hello from new world terminal");
 });
