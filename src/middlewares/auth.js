ef74173bb0717f77655d9c464897358f6d609399
Vedant-07 <vedantnpatel7@gmail.com>
2025-12-20 02:43:26 +0530

    refactored code for auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,35 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+
 const userAuth = async (req, res, next) => {
   try {
-    //read the token
-    const { token } = req.cookies;
-    if (!token)
-      throw new Error("No token at the moment  from the UserAuth ...");
-    //validator token
+    // Check Authorization header first (for localStorage-based auth)
+    const authHeader = req.headers.authorization;
+    let token;
+    
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    } else if (req.cookies && req.cookies.token) {
+      // Fallback to cookie (for local development)
+      token = req.cookies.token;
+    }
+    
+    if (!token) {
+      throw new Error("No token provided");
+    }
+    
+    // Verify token
     const DecryptMsg = await jwt.verify(token, process.env.JWT_SECRET);
     const { _id } = DecryptMsg;
-    //Identify the user
+    
+    // Find user
     const user = await User.findById(_id);
-    console.log("user is ", user);
-    if (!user) throw new Error("No such user is found from the UserAuth ... ");
+    if (!user) throw new Error("No such user found");
+    
     req.user = user;
     next();
   } catch (err) {
-    res.status(401).send("Error Here exclusively from UserAuth  : " + err.message);
+    res.status(401).send("Unauthorized: " + err.message);
   }
 };
 
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -9,49 +9,40 @@ authRouter.post("/signup", async (req, res) => {
     checkValidation(req.body);
     const saltRounds = 7;
     const newPwd = await bcrypt.hash(req.body.password, saltRounds);
-    console.log(newPwd);
     req.body.password = newPwd;
     const user = new User(req.body);
     const signedUpUser = await user.save();
-    res.cookie("token", signedUpUser.getJWT(), {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 3600000
+    const token = signedUpUser.getJWT();
+    
+    res.status(200).json({
+      user: signedUpUser,
+      token: token
     });
-    res.status(200).send(signedUpUser);
   } catch (err) {
-    console.log("error happened in db", err);
-    res.status(400).send("user didnt signed up " + err);
+    res.status(400).send("Signup failed: " + err.message);
   }
 });
 
 authRouter.post("/signin", async (req, res) => {
   try {
     const user = await User.findOne({ emailID: req.body.emailID });
-    if (!user) throw new Error("email doesnt exists");
+    if (!user) throw new Error("Email doesn't exist");
     const checkUser = await user.isPasswordValidated(req.body.password);
-    if (!checkUser) throw new Error("invalid password");
-    res.cookie("token", user.getJWT(), {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 3600000
+    if (!checkUser) throw new Error("Invalid password");
+    
+    const token = user.getJWT();
+    
+    res.json({
+      user: user,
+      token: token
     });
-    res.send(user);
   } catch (e) {
-    res.status(404).send("error : " + e);
+    res.status(404).send("Error: " + e.message);
   }
 });
 
 authRouter.post("/signout", (req, res) => {
-  res.cookie("token", null, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    expires: new Date(0)
-  });
-  res.send("user is signed out.......");
+  res.send("User signed out");
 });
 
 module.exports = authRouter;
